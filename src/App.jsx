70c638500421abb5b753977b5f6d3ad17c709423
eartykbaev <erik.2405@mail.ru>
2023-06-26 18:07:09 +0600

    fixed commits and changed things that broke app

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import MainMenu from "./Components/MainMenu/MainMenu";
-import ScrollableDishMenu from "./SkrollDishMenu";
 import BarMenu from "./Components/BarMenu/BarMenu";
 import DishDetails from "./Components/DishDetails/DishDetails";
 import BarDetails from "./Components/BarDetails/BarDetails";
+import DishMenu from "./Components/DishMenu/DishMenu.jsx";
 
 function App() {
   return (
@@ -13,7 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<MainMenu />} />
         <Route path="/BarMenu/" element={<BarMenu />} />
-        <Route path="/DishMenu/" element={<ScrollableDishMenu />} />
+        <Route path="/DishMenu/" element={<DishMenu />} />
         <Route path="/dish/:id" element={<DishDetails />} />
         <Route path="/bar/:id" element={<BarDetails />} />
       </Routes>
