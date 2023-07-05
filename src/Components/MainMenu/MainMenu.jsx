c4821d83a88db2f435f3b847fa5dfd475dc18660
eartykbaev <erik.2405@mail.ru>
2023-07-06 02:29:58 +0600

    main menu done

--- src/Components/MainMenu/MainMenu.jsx
+++ src/Components/MainMenu/MainMenu.jsx
@@ -1,31 +1,22 @@
-import React, { useEffect, useRef } from "react";
+import React from "react";
 import { NavLink } from "react-router-dom";
+
+import plate from '../../assets/backgrounds/plate.png';
+import bottle from '../../assets/backgrounds/bottle.png';
+
 import style from "../MainMenu/MainMenu.module.css";
 
+
 const MainMenu = () => {
-  const videoRef = useRef(null);
-
-  useEffect(() => {
-    videoRef.current.addEventListener("loadedmetadata", handleVideoLoaded);
-  }, []);
-
-  const handleVideoLoaded = () => {
-    videoRef.current.play();
-  };
-
   return (
     <div className={style.container}>
       <div className={style.leftSection}>
         <NavLink to={"/BarMenu/"} className={style.navItem}></NavLink>
       </div>
-      <video
-        ref={videoRef}
-        className={style.roundVideo}
-        src="/circle.mp4"
-        autoPlay
-        loop
-        muted
-      ></video>
+
+      <img src={bottle} className={style.bottle} alt="plate"/>
+      <img src={plate} className={style.plate} alt="plate"/>
+
       <div className={style.rightSection}>
         <NavLink to={"/DishMenu/"} className={style.navItem}></NavLink>
       </div>
