15ae1af030d83d7b35db37d84ff2525a5f9d9a99
eartykbaev <erik.2405@mail.ru>
2023-07-12 01:55:41 +0600

    added new animation in main menu

--- src/Components/MainMenu/MainMenu.jsx
+++ src/Components/MainMenu/MainMenu.jsx
@@ -1,29 +1,71 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
+import cn from "classnames";
 
-import plate from '../../assets/backgrounds/plate.png';
-import bottle from '../../assets/backgrounds/bottle.png';
-import blackLogo from '../../assets/backgrounds/black-logo.png'
+import left from "../../assets/backgrounds/left.png";
+import center from "../../assets/backgrounds/center.jpg";
+import right from "../../assets/backgrounds/right.jpg";
+import blackLogo from "../../assets/backgrounds/black-logo.png";
 
 import style from "../MainMenu/MainMenu.module.css";
 
-
 const MainMenu = () => {
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoaded(true);
+    }, 2000);
+  }, []);
+
   return (
     <div className={style.container}>
-      <div className={style.leftSection}>
-        <NavLink to={"/bar-menu/"} className={style.navItem}></NavLink>
+        {/*<NavLink to={"/bar-menu/"} className={style.navItem}></NavLink>*/}
+      <div
+        className={cn(style.container_circle, {
+          [style.done]: isLoaded,
+        })}
+      >
+        <img src={blackLogo} className={style.circle} alt="logo" />
       </div>
-
-      <div className={style.container_circle}>
-        <img src={blackLogo} className={style.circle} alt="logo"/>
-      </div>
-
-      <img src={bottle} className={style.bottle} alt="plate"/>
-      <img src={plate} className={style.plate} alt="plate"/>
-
-      <div className={style.rightSection}>
-        <NavLink to={"/dish-menu/"} className={style.navItem}></NavLink>
+      <div className={style.bg_imgs}>
+        <NavLink
+          to="/bar-menu"
+          className={cn(style.bg_imgs_wrap, {
+            [style.left]: true,
+            [style.done]: isLoaded,
+          })}
+        >
+          <img src={left} alt="yellow" className={style.bg_img} />
+          <div>
+            <span className={style.bg_img_title}>БАР</span>
+          </div>
+        </NavLink>
+        <NavLink
+          to="dish-menu"
+          className={cn(style.bg_imgs_wrap, {
+            [style.center]: true,
+            [style.done]: isLoaded,
+          })}
+        >
+          <img src={center} alt="green" className={style.bg_img} />
+          <div>
+            <span className={style.bg_img_title}>МЕНЮ</span>
+          </div>
+        </NavLink>
+        <div
+          className={cn(style.bg_imgs_wrap, {
+            [style.right]: true,
+            [style.done]: isLoaded,
+          })}
+        >
+          <img src={right} alt="blue" className={style.bg_img} />
+          <div>
+            <span className={style.bg_img_title_wine}>ВИННАЯ</span>
+            <br />
+            <span className={style.bg_img_title}>КАРТА</span>
+          </div>
+        </div>
       </div>
     </div>
   );
