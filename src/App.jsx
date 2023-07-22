a3cd5f49b7e85b594c3b9dcfa0b905411b1291bd
WIN-2QJDOS8CRCD\user <baxxromm@gmail.com>
2023-07-22 16:09:44 +0600

    remove some components

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import MainMenu from "./Components/MainMenu/MainMenu";
 import BarMenu from "./Components/BarMenu/BarMenu";
-import DishDetails from "./Components/DishDetails/DishDetails";
-import BarDetails from "./Components/BarDetails/BarDetails";
 import DishMenu from "./Components/DishMenu/DishMenu.jsx";
 import WineCard from "./Components/WineCard/WineCard";
+// import DishDetails from "./Components/DishDetails/DishDetails";
+// import BarDetails from "./Components/BarDetails/BarDetails";
 
 function App() {
   return (
@@ -16,9 +16,9 @@ function App() {
         <Route path="/bar-menu/" element={<BarMenu />} />
         <Route path="/wine-card/" element={<WineCard />} />
         <Route path="/dish-menu/" element={<DishMenu />} />
-        <Route path="/dish/:id" element={<DishDetails />} />
-        <Route path="/bar/:id" element={<BarDetails />} />
-        <Route path="/wine/:id" element={<WineCard />} />
+        {/* <Route path="/wine/:id" element={<WineCard />} /> */}
+        {/* <Route path="/dish/:id" element={<DishDetails />} />
+        <Route path="/bar/:id" element={<BarDetails />} /> */}
       </Routes>
     </>
   );
